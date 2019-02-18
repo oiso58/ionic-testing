c79ccbd96de83d8e78df072332f7d1230c493992
Mona MODI <oiso58@gmail.com>
2019-02-18 10:03:47 +0100

    First commit

---
+++ src/app/appareils/appareils.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareils',
+  templateUrl: './appareils.page.html',
+  styleUrls: ['./appareils.page.scss'],
+})
+export class AppareilsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
